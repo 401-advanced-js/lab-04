1939bed9f61c3137395d7c08ba31f010df2de230
shiratap <spencerhirata@gmail.com>
2019-03-21 21:15:02 -0700

    unfinished business

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const fileWriter = require('./lib/fileWriter.js');
+
+// Obtain and assert input
+let files = process.argv.slice(2);
+
+// if( ! (files instanceof Array && files.length !== 1) ) {
+//   throw new Error('Invalid Args');
+// }
+
+fileWriter(files[0], (err,data) => {
+  if ( err ) { throw err; }
+});
